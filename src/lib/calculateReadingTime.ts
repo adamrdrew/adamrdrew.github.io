2e30de8b67bf7f4ac6cd0f849b204de8046cec45
Adam Drew <addrew@redhat.com>
2026-02-21 13:10:01 -0500

    Back to empty

--- src/lib/calculateReadingTime.ts
+++
@@ -1,5 +0,0 @@
-export default function calculateReadingTime(text: string): number {
-  const wordsPerMinute = 200;
-  const wordCount = text.trim().split(/\s+/).length;
-  return Math.ceil(wordCount / wordsPerMinute);
-}
